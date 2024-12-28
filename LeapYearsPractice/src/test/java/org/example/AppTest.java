f2066cb2359f02ac136db60e620d0249a9bdf360
KARTIK DHANDHALA <kartikpaliwal991@gmail.com>
2024-12-28 21:48:03 +0530

    Here we can see that Step-1 of TDD we can't find LeapYear function so we need to stop creating test cases and write production code

--- LeapYearsPractice/src/test/java/org/example/AppTest.java
+++ LeapYearsPractice/src/test/java/org/example/AppTest.java
@@ -1,7 +1,7 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
+import junit.framework.Test;//An interface that represents a test in JUnit framework
+import junit.framework.TestCase;//A base class for creating test cases
 import junit.framework.TestSuite;
 
 /**
@@ -10,27 +10,20 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
+    
     public AppTest( String testName )
     {
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
+    public  void leapYearDivisibleBy400(){
+        assertTrue(LeapYear.isLeapYear(4000));
+    }
     public void testApp()
     {
         assertTrue( true );
