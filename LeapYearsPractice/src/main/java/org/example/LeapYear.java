33855581a6f45c9cab0470e4c2e2f3966b6117a0
KARTIK DHANDHALA <kartikpaliwal991@gmail.com>
2024-12-28 22:05:46 +0530

    Again it fails so we have to stop generating test cases and should generate production code by rule 2 of TDD

--- LeapYearsPractice/src/main/java/org/example/LeapYear.java
+++ LeapYearsPractice/src/main/java/org/example/LeapYear.java
@@ -3,6 +3,8 @@ package org.example;
 public class LeapYear {
     public static boolean isLeapYear(int year){
         if(year%400==0)return true;
-        else return false;
+        if(year%4==0 && year%100!=0)return true;
+        else  return false;
+        
     }
 }
