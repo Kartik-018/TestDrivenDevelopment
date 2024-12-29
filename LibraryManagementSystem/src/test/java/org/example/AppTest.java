06423fb27c38d33f0ee4ba20cd52cda0d9ccf5c2
KARTIK DHANDHALA <kartikpaliwal991@gmail.com>
2024-12-29 09:29:32 +0530

    Here we can't find addBook Method so  we have to write production code

--- LibraryManagementSystem/src/test/java/org/example/AppTest.java
+++ LibraryManagementSystem/src/test/java/org/example/AppTest.java
@@ -3,6 +3,7 @@ package org.example;
 
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class AppTest {
     private Library library;
@@ -10,5 +11,9 @@ public class AppTest {
     void setup(){
          library=new Library();
      }
+     @Test
+    void testAddBookUsingArray() {
+        library.addBook(new String[]{"1234", "The forgotten sons", "Trinanjan Chakraborty", "2020"}, 1);
 
+    }
 }
